75f57a1060cffd67c81451ecacf24de79d2c3b8d
szajbergyerek <balint.daniel623@gmail.com>
2026-03-20 05:45:22 +0100

    Git actions

--- setup.py
+++ setup.py
@@ -5,6 +5,8 @@ setup(
     version="1.0.2",
     description="Removes torrents with data after a configurable seed time.",
     author="szajbergyerek",
+    author_email="dani@balintdaniel.com",
+    url="http://deluge-torrent.org",
     packages=find_packages(),
     package_data={"autoremovelite": ["data/*.js"]},
     entry_points={
